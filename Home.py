0f159b00f389d91dd561cd7bd94650651333f853
Quera <kevin.duranty@quera.fr>
2025-04-29 12:04:58 +0200

    config page

--- Home.py
+++ Home.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
 
+# Configuration de la page
+st.set_page_config(layout="wide", #centered,
+                   page_title="Présentation de Streamlit",
+                   page_icon="👋",
+                   )
+
 # Titre
 st.title('Présentation de Streamlit')
